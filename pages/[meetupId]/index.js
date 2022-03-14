ca6680dd749cd6400e24d54d7057f099e25cc0ed
puja.rani <puja.rani@ril.com>
2022-03-14 07:31:51 +0530

    feat: Use getStaticPaths to speicy all possible dynamic routes

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -10,5 +10,40 @@ const MeetupDetails = () => {
     />
   );
 };
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  //fetch data for a single meetup
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/4/45/Standbeeld_in_Oostende.jpg",
+        title: "A First Meetup",
+        address: "Somes treet 5 , some city",
+        description: "The meetup description",
+      },
+    },
+  };
+}
 
 export default MeetupDetails;
--- pages/index.js
+++ pages/index.js
@@ -23,25 +23,25 @@ function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
 }
 
-export async function getServerSideProps(context) {
-  const req = context.req;
-  const res = context.res;
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
 
-  //fetch data from api
-
-  return {
-    props: {
-      meetups: DUMMY_MEETUPS,
-    },
-  };
-}
-
-// export async function getStaticProps() {
 //   //fetch data from api
+
 //   return {
-//     props: { meetups: DUMMY_MEETUPS },
-//     revalidate: 1,
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
 //   };
 // }
+
+export async function getStaticProps() {
+  //fetch data from api
+  return {
+    props: { meetups: DUMMY_MEETUPS },
+    revalidate: 1,
+  };
+}
 
 export default HomePage;
