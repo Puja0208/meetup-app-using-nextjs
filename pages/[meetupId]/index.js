05170be41a124f452d94373a3fe037cf5ad53a32
puja.rani <puja.rani@ril.com>
2022-03-14 05:47:42 +0530

    feat: Add styles to meetup detail page

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/4/45/Standbeeld_in_Oostende.jpg"
+      title="A First Meetup"
+      address="Somes treet 5 , some city"
+      description="The meetup description"
+    />
+  );
+};
+
+export default MeetupDetails;
