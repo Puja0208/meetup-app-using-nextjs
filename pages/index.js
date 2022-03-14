34a70ecca25579581d90da8491792a1dd876a98d
puja.rani <puja.rani@ril.com>
2022-03-14 06:57:37 +0530

    feat: Use getServerSideProps to populate meetups list

--- pages/index.js
+++ pages/index.js
@@ -23,12 +23,25 @@ function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps(context) {
+  const req = context.req;
+  const res = context.res;
+
   //fetch data from api
+
   return {
-    props: { meetups: DUMMY_MEETUPS },
-    revalidate: 10,
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
   };
 }
+
+// export async function getStaticProps() {
+//   //fetch data from api
+//   return {
+//     props: { meetups: DUMMY_MEETUPS },
+//     revalidate: 1,
+//   };
+// }
 
 export default HomePage;
